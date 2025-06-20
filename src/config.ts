b95520638551fcb4564969d93330b1d2e0afaeae
Vitinho0 <75380202+Vitinho0@users.noreply.github.com>
2025-06-20 15:50:20 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { ServerOptions } from './types/ServerOptions';
 
 export default {
-  secretKey: 'THISISMYSECURETOKEN',
+  secretKey: 'boterp',
   host: 'http://localhost',
   port: '21465',
   deviceName: 'WppConnect',
